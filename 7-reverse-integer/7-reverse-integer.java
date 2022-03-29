464253f4b4d9c308ff4ec59e8857e2670b9852ab
James Chen <qgjameschen@gmail.com>
2022-03-28 22:01:58 -0400

    Time: 4 ms (15.44%), Space: 41.7 MB (35.80%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int reverse(int x) {
+        boolean isNegative = false;
+        int newVal = 0;
+        int oldVal = x;
+        
+        if (oldVal < 0) {
+            oldVal *= -1;
+            isNegative = true;
+        }
+        ArrayList<Character> digits = new ArrayList<>();
+        
+        char[] oldValArr = String.valueOf(oldVal).toCharArray();
+        
+        char[] newValArr = new char[oldValArr.length];
+        
+        for (int i = oldValArr.length - 1; i >= 0; i--) {
+            newValArr[oldValArr.length - 1 - i] = oldValArr[i];
+        }
+        
+        try {
+            newVal = Integer.parseInt(String.valueOf(newValArr));
+        }
+        catch (NumberFormatException e) {
+            return 0;
+        }
+        
+
+        if(newValArr.length != oldValArr.length) {
+            return 0;
+        }
+        
+            
+        if(isNegative) {
+            newVal *= -1;
+        }
+        
+        return newVal;
+    }
+}
