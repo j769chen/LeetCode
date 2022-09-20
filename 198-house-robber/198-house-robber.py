a312756f8049d90b770854274602995f84b4670c
James Chen <qgjameschen@gmail.com>
2022-09-19 21:39:12 -0400

    Time: 60 ms (28.29%), Space: 13.9 MB (65.02%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,9 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        firstPrev, secondPrev = 0, 0
-        
-        for i in nums:
-            curr = max(i + secondPrev, firstPrev)
-            secondPrev = firstPrev
-            firstPrev = curr
-        
-        return curr
-            
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        max_3_house_before, max_2_house_before, adjacent = 0, 0, 0
+        for cur in num:
+            max_3_house_before, max_2_house_before, adjacent = \
+            max_2_house_before, adjacent, max(max_3_house_before+cur, max_2_house_before+cur)
+        return max(max_2_house_before, adjacent)
