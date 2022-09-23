f908ef51d3690a323496b13560c278c327493378
James Chen <qgjameschen@gmail.com>
2022-09-22 22:18:21 -0400

    Time: 98 ms (29.40%), Space: 14.4 MB (96.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,28 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s1 = {}
-        s2 = {}
-        
-        for c in s:
-            if s1.get(c) is None:
-                s1[c] = 1
-            else:
-                s1[c] = s1.get(c) + 1
-                
-        for c in t:
-            if s2.get(c) is None:
-                s2[c] = 1
-            else:
-                s2[c] = s2.get(c) + 1
-                
-        if len(s1) > len(s2):
-            for x in s1.keys():
-                if (s1.get(x) != s2.get(x)):
-                    return False
-        else:
-            for x in s2.keys():
-                if (s2.get(x) != s1.get(x)):
-                    return False
-                
-        return True
+            tracker = collections.defaultdict(int)
+            for x in s: tracker[x] += 1
+            for x in t: tracker[x] -= 1
+            return all(x == 0 for x in tracker.values())
             
