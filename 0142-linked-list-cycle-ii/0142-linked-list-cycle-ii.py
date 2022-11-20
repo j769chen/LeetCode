aa1d9af5d028ae1b4a1c6c95e466df0b5e685198
James Chen <qgjameschen@gmail.com>
2022-11-20 14:21:53 -0500

    Time: 107 ms (48.70%), Space: 17.3 MB (61.84%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -4,10 +4,14 @@
 #         self.val = x
 #         self.next = None
 
+# Floyd's algorithm, tortois and hare, slow pointer goes 1 node at a time, fast pointer goes 2
+# Once they intersect, the distance from that intersection to the start of the cycle is same dist as 
+# Head node to start of cycle
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or head.next is None:
             return None 
+        
         slowPointer, fastPointer = head, head
         
         while True:
