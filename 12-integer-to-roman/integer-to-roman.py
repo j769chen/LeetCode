cc10301b74817cba5c8751e83139d49156c76451
James Chen <qgjameschen@gmail.com>
2023-10-01 22:21:26 -0400

    Time: 55 ms (43.81%) | Memory: 16.3 MB (73.99%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,64 +1,37 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        dict = {
+            1: "I",
+            4: "IV",
+            5: "V",
+            9: "IX",
+            10: "X",
+            40: "XL",
+            50: "L",
+            90: "XC",
+            100: "C",
+            400: "CD",
+            500: "D",
+            900: "CM",
+            1000: "M"
+        }
         solString = ""
     # largestToCheck = 1000
     # if largestToCheck ==
-        if num >= 1000:
-            temp = num//1000
-            for i in range(temp):
-                    solString += "M"
-            num = num - (temp*1000)
-        
-        if num >= 900:
-            solString += "CM"
-            num = num - 900
-        
-        if num >= 500:
-            solString += "D"
-            num = num - 500
-        
-        if num >= 400:
-            solString += "CD"
-            num = num - 400
-        
-        if num >= 100:
-            temp = num//100
-            for i in range(temp):
-                solString += "C"
-            num = num - (temp*100)
-        
-        if num >= 90:
-            solString += "XC"
-            num = num - 90
-            
-        if num >= 50:
-            solString += "L"
-            num = num - 50
-            
-        if num >= 40:
-            solString += "XL"
-            num = num - 40
-        
-        if num >= 10:
-            temp = num//10
-            for i in range(temp):
-                solString += "X"
-            num = num - (temp*10)
-        
-        if num >= 9:
-            solString += "IX"
-            num = num - 9
-        
-        if num >= 5:
-            solString += "V"
-            num = num - 5
-            
-        if num >= 4:
-            solString += "IV"
-            num = num - 4
-        
-        if num >= 1:
-            for i in range(num):
-                solString += "I"
-
+        solString = ""
+        numsToCheck = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    
+        for i in numsToCheck:
+            if num >= i:
+                if i == 1000 or i == 100 or i == 10:
+                    temp = num//i
+                    for j in range(temp):
+                        solString += dict[i]
+                    num -= temp * i
+                elif i == 1:
+                    for j in range(num):
+                        solString += dict[i]
+                else:
+                    solString += dict[i]
+                    num -= i
         return solString
