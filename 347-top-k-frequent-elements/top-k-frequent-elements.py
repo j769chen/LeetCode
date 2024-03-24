c5713b012854575101e1f646fb3d2738cac032d9
James Chen <qgjameschen@gmail.com>
2024-03-23 22:36:57 -0400

    Time: 103 ms (9.91%) | Memory: 22.1 MB (14.01%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,28 @@
+class Solution:
+    # bucket sort, we first get the frequencies of each element, then place them into buckets 
+    # based on their frequency. We know that the max number of times an element can be in 
+    # in the array is the size of the array, so we need a bucket for 0 -> len(arr) frequencies
+    # then we can simply go through the buckets and get the top k
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == 1:
+            return nums
+        
+        elts = {}
+
+        for n in nums:
+            if n in elts:
+                elts[n] += 1
+            else:
+                elts[n] = 1
+        
+        buckets = [[] for i in range(len(nums)+1)]
+        
+        for i in elts.keys():
+            buckets[elts[i]].append(i)
+        
+        kElts = []
+        for i in range(len(buckets)-1, 0, -1):
+            for j in buckets[i]:
+                kElts.append(j)
+                if len(kElts) == k:
+                    return kElts
