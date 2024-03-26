3aa687f6614ff99fd21e4d727e110ca65c2c9140
James Chen <qgjameschen@gmail.com>
2024-03-25 21:58:25 -0400

    Time: 49 ms (32.82%) | Memory: 17 MB (79.05%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        top = 0
+        bottom = len(matrix) - 1
+
+        while top <= bottom:
+            mid = bottom-top//2
+            print(mid)
+            if target <= matrix[mid][-1] and target >= matrix[mid][0]:
+                print(matrix[0])
+                return self.search(matrix[mid], target)
+            elif target > matrix[mid][-1]:
+                top = mid + 1
+            else:
+                bottom = mid - 1
+
+    # binary search for when we find the right array
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = right-left//2
+            if target == nums[mid]:
+                return True
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
