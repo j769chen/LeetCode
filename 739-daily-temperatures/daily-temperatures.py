92c2107c45cf89fc54e544299797cc8ce9daa6c8
James Chen <qgjameschen@gmail.com>
2024-04-07 22:10:05 -0400

    Time: 870 ms (87.21%) | Memory: 31.2 MB (52.69%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,5 +1,10 @@
 class Solution:
+    
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # declare an empty stack and a solution arr thats the length of the og arr but all 0s
+        # iterate through array by idx, while stack not empty and the top elt of stack is less than 
+        # temp at curr idx, pop it, solution at that idx is curr idx - temp_idx
+        # append the curr idx to the stack
         stack = []
         sol = [0] * len(temperatures)
 
@@ -10,16 +15,3 @@ class Solution:
             stack.append(i)
         
         return sol
-
-# class Solution:
-#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-#         stack = []
-#         ans = [0] * len(temperatures)
-        
-#         for i in range(len(temperatures)):
-#             while stack and temperatures[stack[-1]] < temperatures[i]:
-#                 ind = stack.pop()
-#                 ans[ind] = (i - ind)
-#             stack.append(i)
-
-#         return ans
