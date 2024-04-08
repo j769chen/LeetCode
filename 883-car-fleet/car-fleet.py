df2b4fdc238849b9df5cf23bd63184bece20ec4c
James Chen <qgjameschen@gmail.com>
2024-04-07 22:26:38 -0400

    Time: 649 ms (87.77%) | Memory: 38.8 MB (49.23%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,6 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #First, create a list of tuples containing position and speed, sorted (i.e. first by position, 
+        # then ties broken by speed). Then create a list of each car's time to destination based on 
+        # values in the list of tuples (timeToDest.append((target-t[0])/t[1])). Finally, iterate 
+        # through the time to destination list in reverse order, and if the current time is greater than 
+        # the current slowest time, that is one more car fleet.
         carMap = sorted(zip(position, speed))
+
         timeToDest = []
 
         for t in carMap:
